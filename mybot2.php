b96f819bf7a0f1e3ce9ba26f235a6bb398f494a3
sasanm2 <46301571+sasanm2@users.noreply.github.com>
2019-01-01 22:27:48 +0300

    Rename mybot.php to mybot2.php

---
+++ mybot2.php
@@ -0,0 +1,7 @@
+
+date_default_timezone_set("asia/tehran");
+// Set the bot TOKEN
+$bot_id = "691608015:AAHuNzY4wE-dEQjwMf84B4TYGKS68481tFg
+";
+// Instances the class
+$telegram = new Telegram($bot_id);
